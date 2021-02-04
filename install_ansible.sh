1fb4040a489cc45bfb75409cfd14aed27f628edf
Steven Lee <shl1@cornell.edu>
2021-02-04 20:48:37 +0000

    specify opensdk version 0.52.0

--- install_ansible.sh
+++ install_ansible.sh
@@ -4,6 +4,6 @@ python3 -m venv ansible
 source ./ansible/bin/activate
 
 pip install --upgrade pip wheel
-pip install --upgrade ansible python-openstackclient openstacksdk
+pip install --upgrade ansible python-openstackclient openstacksdk==0.52.0
 
 ansible-galaxy install -r requirements.yml
